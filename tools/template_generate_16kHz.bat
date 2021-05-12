93609e545ddbf0f0b742187d24bbcebc4d8cf6ba
Wudi <wudicgi@gmail.com>
2021-05-12 22:25:20 +0800

    Made the template_generate_16kHz.bat batch script work for incomplete model files

--- tools/template_generate_16kHz.bat
+++ tools/template_generate_16kHz.bat
@@ -1,20 +1,52 @@
-@echo ========================= 2stems-16kHz =========================
-mkdir 2stems-16kHz
-xcopy /i 2stems\variables 2stems-16kHz\variables
-copy /b model_files-16kHz\2stems-16kHz-saved_model.pb 2stems-16kHz\saved_model.pb
-@echo.
+@echo off
 
-@echo ========================= 4stems-16kHz =========================
-mkdir 4stems-16kHz
-xcopy /i 4stems\variables 4stems-16kHz\variables
-copy /b model_files-16kHz\4stems-16kHz-saved_model.pb 4stems-16kHz\saved_model.pb
-@echo.
+:: using xcopy to copy single file
+:: https://superuser.com/questions/34303/xcopy-not-surpressing-file-directory-query/540671#540671
+:: https://ss64.com/nt/xcopy.html (Copy a single file)
 
-@echo ========================= 5stems-16kHz =========================
-mkdir 5stems-16kHz
-xcopy /i 5stems\variables 5stems-16kHz\variables
-copy /b model_files-16kHz\5stems-16kHz-saved_model.pb 5stems-16kHz\saved_model.pb
-@echo.
+:: using "copy /b" instead of xcopy will display indent spaces
 
-@echo Completed.
-@echo.
+echo ========================= 2stems-16kHz =========================
+if exist "2stems" (
+    if not exist "2stems-16kHz" (
+        mkdir 2stems-16kHz
+        xcopy /i 2stems\variables 2stems-16kHz\variables
+        xcopy model_files-16kHz\2stems-16kHz-saved_model.pb 2stems-16kHz\saved_model.pb*
+    ) else (
+        echo Folder 2stems-16kHz is already existed, skip
+    )
+) else (
+    echo Folder 2stems does not exist, skip
+)
+echo.
+
+echo ========================= 4stems-16kHz =========================
+if exist "4stems" (
+    if not exist "4stems-16kHz" (
+        mkdir 4stems-16kHz
+        xcopy /i 4stems\variables 4stems-16kHz\variables
+        xcopy model_files-16kHz\4stems-16kHz-saved_model.pb 4stems-16kHz\saved_model.pb*
+    ) else (
+        echo Folder 4stems-16kHz is already existed, skip
+    )
+) else (
+    echo Folder 4stems does not exist, skip
+)
+echo.
+
+echo ========================= 5stems-16kHz =========================
+if exist "5stems" (
+    if not exist "5stems-16kHz" (
+        mkdir 5stems-16kHz
+        xcopy /i 5stems\variables 5stems-16kHz\variables
+        xcopy model_files-16kHz\5stems-16kHz-saved_model.pb 5stems-16kHz\saved_model.pb*
+    ) else (
+        echo Folder 5stems-16kHz is already existed, skip
+    )
+) else (
+    echo Folder 5stems does not exist, skip
+)
+echo.
+
+echo Completed.
+echo.
